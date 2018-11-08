93004fc5a1bf350c6055b3d980ff59c8039b0158
vmtprogrammer <43457469+vmtprogrammer@users.noreply.github.com>
2018-11-08 20:30:35 -0200

    Create usodeimagem.js

---
+++ usodeimagem.js
@@ -0,0 +1,75 @@
+const contratoUsoDeImagemABI = [
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "nomeEmpresa",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "receberPeloUso",
+		"outputs": [],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "qualAgente",
+				"type": "address"
+			}
+		],
+		"name": "definirAgente",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "qualNomeDaEmpresa",
+				"type": "string"
+			}
+		],
+		"name": "definirNomeDaEmpresa",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	}
+];
+
+var contratoUsoDeImagem = window.conexaoComEthereum.eth.contract(contratoUsoDeImagemABI).at("0xaf897e8de482012763f009218042b8f937518b35");
+
+function obtemNomeEmpresa() {
+    contratoUsoDeImagem.nomeEmpresa({from: contaUsuario, gas: 3000000, value: 0}, function (err, resultado) {
+        if (err)    {
+            console.log("Erro");
+            console.error(err);
+        } else {
+            console.log("Resultado");
+            let objStatus = document.getElementById("spanNomeEmpresa");
+            console.log(resultado);
+            objStatus.innerText = resultado;
+        }
+    });
+}
